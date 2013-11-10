c903ec767b1c071a53a254be26bc3b12f50929b0
LEETAEHO <tae1560@gmail.com>
2013-11-11 00:04:17 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewPoolcOgameServer::Application.config.secret_key_base = '2dd31af33fd070e8cb8d52a90641f7181e7250de47df82387551756f58fffef2cb3f3d02756a3538648cff342d9aa34b655ba69206d255738f157524fc2245d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewPoolcOgameServer::Application.config.session_store :cookie_store, key: '_new_poolc_ogame_server_session'
